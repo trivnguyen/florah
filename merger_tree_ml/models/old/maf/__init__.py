ee2cb9cb50ff6f7a998607bfc8ba6798678b6cfa
Tri Nguyen <tnguyen@ccalin011.flatironinstitute.org>
2022-11-22 18:16:11 -0500

    remove old code

--- merger_tree_ml/models/old/maf/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from .data_module import DataModule
-from .transform import Transform
-from .model import MergerTreeMAF as Model
--- merger_tree_ml/models/old/sa_maf/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from .data_module import DataModule
-from .transform import Transform
-from .model import AttentionMAF as Model
