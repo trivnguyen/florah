45e01fb4dd95db299715e870318988a50ca0fb58
Tri Nguyen <tnguyen@ccalin011.flatironinstitute.org>
2023-08-29 15:54:28 -0400

    remove experimental features

--- src/florah/models/transforms.py
+++ src/florah/models/transforms.py
@@ -1,7 +1,9 @@
-from typing import Optional, List, Tuple
 
-import torch
+from typing import List, Optional, Tuple
+
 import numpy as np
+import torch
+
 
 class StandardScaler(torch.nn.Module):
     """ Standard scaling transformation """
@@ -145,81 +147,6 @@ class Preprocess(torch.nn.Module):
         else:
             x_padded = self.x_scaler.transform(x_padded)
             y_padded = self.y_scaler.transform(y_padded)
-            t_padded = self.t_scaler.transform(t_padded)
-
-        return x_padded, y_padded, t_padded, seq_len, mask
-
-
-class PreprocessClassifier(torch.nn.Module):
-    """ Preprocessing module for merger tree data """
-    def __init__(
-            self, nx: int, nt: int = 1,
-            sub_dim: Optional[List[int]] = None, use_dt: bool = False,
-            use_t: bool = False) -> None:
-        """
-        Parameters
-        ----------
-            nx: int
-                Number of dimensions for input series X
-            nt: int
-                Number of dimensions for time series T. Default: 1
-            use_dt: bool
-                Deprecated.
-            use_t: bool
-                Deprecated.
-        """
-        super(PreprocessClassifier, self).__init__()
-
-        self.x_scaler = StandardScaler(nx, dim=[0, 1])
-        self.t_scaler = StandardScaler(nt, dim=[0, 1])
-        self.use_t = True
-        self.sub_dim = sub_dim if sub_dim is not None else []
-
-    def forward(self, *args, **kargs):
-        """ Apply transformation """
-        return self.transform(*args, **kargs)
-
-    def transform(self, data: dict, fit: bool = True) -> Tuple[()]:
-        """ Apply transformation
-        Parameters:
-            data: dict
-                Data dictionary with key "x" and "t"
-            fit: bool
-                If True, fit scaler to data and then transform. Default: True
-        """
-        # get input and output series from data
-        # output is input shifted right
-        x = [tree[:-1] for tree in data.get('x')]
-        y = [tree[:-1] for tree in data.get('y')]
-        t = data.get('t')
-
-        # pad sequence to maximum length
-        seq_len = np.array([len(x[i]) for i in range(len(x))])
-        max_len = np.max(seq_len)
-        x_padded = np.zeros((len(x), max_len, *x[0].shape[1:]))
-        y_padded = np.zeros((len(x), max_len, *y[0].shape[1:]))
-        t_padded = np.zeros((len(x), max_len + 1, *t[0].shape[1:]))
-        mask = np.zeros((len(x), max_len), dtype=np.bool)
-
-        for i in range(len(x)):
-            x_padded[i, :seq_len[i]] = x[i]
-            y_padded[i, :seq_len[i]] = y[i]
-            t_padded[i, :seq_len[i] + 1] = t[i]
-            mask[i, :seq_len[i]] = True
-
-        # convert to tensor
-        x_padded = torch.tensor(x_padded, dtype=torch.float32)
-        y_padded = torch.tensor(y_padded, dtype=torch.float32)
-        t_padded = torch.tensor(t_padded, dtype=torch.float32)
-        seq_len = torch.tensor(seq_len, device='cpu', dtype=torch.int64)
-        mask = torch.tensor(mask, dtype=torch.bool)
-
-        # apply scaler as the last step
-        if fit:
-            x_padded = self.x_scaler.fit_transform(x_padded)
-            t_padded = self.t_scaler.fit_transform(t_padded)
-        else:
-            x_padded = self.x_scaler.transform(x_padded)
             t_padded = self.t_scaler.transform(t_padded)
 
         return x_padded, y_padded, t_padded, seq_len, mask
